00fa87ebe42a9bc7ee4153f8cbe3544949839c49
Vas <mobil5662016@gmail.com>
2020-02-10 16:45:21 +0300

    Add main class Runner

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -1,5 +1,39 @@
 package main;
 
-public class Runner {
+import java.util.ArrayList;
 
+import service.MatrixChangerThread;
+import service.MatrixHolder;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		MatrixHolder matrix = MatrixHolder.MATRIX;
+		ArrayList<Thread> threadList = new ArrayList<Thread>();
+		for (int i = 0; i < 5; i++) {
+			Thread thread = new MatrixChangerThread(i+1); 
+			threadList.add(thread);
+			thread.start();
+		}
+		
+		for (Thread thread : threadList) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}		
+		
+		printMatr(matrix);
+		
+	}
+	
+	public static void printMatr(MatrixHolder matrix) {
+		for (int i = 0; i < matrix.getLength(); i++) {
+			for (int j = 0; j < matrix.getLength(); j++) {
+				System.out.print(matrix.getValueInNoda(i, j) + " ");
+			}
+			System.out.println();
+		}
+	}
 }
